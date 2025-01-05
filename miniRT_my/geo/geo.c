0371d6d6a9e3963fc54afd4629671af1cd6236c1
baouragh <baouragh@e1r9p3.1337.ma>
2025-01-05 19:04:09 +0100

    RGB SO SMOOTH

--- miniRT_my/geo/geo.c
+++ miniRT_my/geo/geo.c
@@ -9,6 +9,9 @@ void print_type(t_tuple *tuple)
     else if (tuple->w == 1)
         printf("is Point : x = %f, y = %f, z = %f , w = %d\n",
             tuple->x, tuple->y, tuple->z, tuple->w);
+    else if (tuple->w == 20)
+        printf("is Color : x = %f, y = %f, z = %f , w = %d\n",
+            tuple->x, tuple->y, tuple->z, tuple->w);
     else
         printf("is Undefined : x = %f, y = %f, z = %f , w = %d\n",
             tuple->x, tuple->y, tuple->z, tuple->w);
@@ -17,7 +20,8 @@ void print_type(t_tuple *tuple)
 // CREATE A TUPLE STRUCTURE---------------------------------------------------------------------
 t_tuple *creat_tuple(double x, double y, double z, double w)
 {
-    t_tuple *tuple =(t_tuple *)malloc(sizeof(t_tuple));
+    t_tuple *tuple;
+    tuple = (t_tuple *)malloc(sizeof(t_tuple));
     if (tuple == NULL)
         return (NULL);
 
@@ -37,6 +41,11 @@ t_tuple *creat_point(double x, double y, double z)
 t_tuple *creat_vector(double x, double y, double z)
 {
     return (creat_tuple(x, y, z, 0));
+}
+
+t_tuple *creat_color(double x, double y, double z)
+{
+    return (creat_tuple(x, y, z, 20));
 }
 
 // ARTHEMATIC FUNCTIONS-------------------------------------------------------------------------
@@ -101,29 +110,21 @@ t_tuple *cross_tuple(t_tuple *a, t_tuple *b)
         a->z * b->x - a->x * b->z,
         a->x * b->y - a->y * b->x));
 }
+
+t_tuple *hadamard_product(t_tuple *a, t_tuple *b)
+{
+    return (creat_color(a->x * b->x, a->y * b->y, a->z * b->z));
+}
 // MAIN FUNCTION--------------------------------------------------------------------------------
 
-typedef struct projectile
-{
-    t_tuple *position;
-    t_tuple *velocity;
-} t_projectile;
-
-typedef struct environment
-{
-    t_tuple *gravity;
-    t_tuple *wind;
-} t_environment;
-
-
-t_projectile *creat_projectile(t_tuple *position, t_tuple *velocity)
+t_projectile *creat_projectile(t_tuple *position, t_tuple *speed)
 {
     t_projectile *projectile = (t_projectile *)malloc(sizeof(t_projectile));
     if (projectile == NULL)
         return (NULL);
 
     projectile->position = position;
-    projectile->velocity = velocity;
+    projectile->speed = speed;
 
     return (projectile);
 }
@@ -142,15 +143,156 @@ t_environment *creat_environment(t_tuple *gravity, t_tuple *wind)
 
 t_projectile *tick(t_environment *env, t_projectile *projectile)
 {
-    t_tuple *position = add_tuple(projectile->position, projectile->velocity);
-    t_tuple *velocity = add_tuple(add_tuple(projectile->velocity, env->gravity), env->wind);
+    t_tuple *position = add_tuple(projectile->position, projectile->speed);
+    t_tuple *speed = add_tuple(add_tuple(projectile->speed, env->gravity), env->wind);
 
-    return (creat_projectile(position, velocity));
+    return (creat_projectile(position, speed));
+}
+
+// MLX MY--------------------------------------------------------------------------------------
+
+t_img *new_img( int width, int height, void *mlx)
+{
+    t_img *img;
+
+    img = (t_img*)malloc(sizeof(t_img));
+    if (!img)
+        return (NULL);
+    img->img = mlx_new_image(mlx, width, height);
+    if (!img->img)
+        return (NULL);
+    img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
+    if (!img->addr)
+        return (NULL);
+    return (img);
+}
+
+t_canvas * creat_canvas(int width , int height, void *mlx_ptr)
+{
+    t_canvas *canvas;
+
+    canvas = (t_canvas *)malloc(sizeof(t_canvas));
+    if (!canvas)
+        return (NULL);
+    canvas->width = width;
+    canvas->height = height;
+    canvas->img = new_img(width, height, mlx_ptr);
+    if (!canvas->img)
+        return (NULL);
+    return (canvas);
+}
+
+int color_at (t_img *img, int x, int y)
+{
+    char    *dst;
+
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	return (*(unsigned int*)dst);
+}
+
+int check_color (t_img *img, int x, int y, int color)
+{
+    char    *dst;
+
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	return (*(unsigned int*)dst == (unsigned int) color);
+}
+
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
+{
+	char    *dst;
+    
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int color_to_int(t_tuple *color)
+{
+    int r;
+    int g;
+    int b;
+
+    r = color->x * 255;
+    g = color->y * 255;
+    b = color->z * 255;
+
+    if (r > 255)
+        r = 255;
+        else if (r < 0)
+            r = 0;
+    if (g > 255)
+        g = 255;
+        else if (g < 0)
+            g = 0;
+    if (b > 255)
+        b = 255;
+        else if (b < 0)
+            b = 0;
+
+    return ((r << 16) | (g << 8) | (b));
+}
+
+t_tuple * int_to_color(int color)
+{
+    double r;
+    double g;
+    double b;
+
+    r = ((color >> 16) & 0XFF) / 255.0;
+    g = ((color >> 8) & 0XFF) / 255.0;
+    b = (color & 0XFF) / 255.0;
+
+    return (creat_color(r, g, b));
+}
+
+void write_pixel(t_canvas *canvas, int x, int y, t_tuple *red)
+{
+    my_mlx_pixel_put(canvas->img, x, y, color_to_int(red));
+}
+
+t_tuple *pixel_at(t_canvas *canvas, int x, int y)
+{ 
+    int color;
+
+    color = color_at(canvas->img, x, y);
+    return (int_to_color(color));
 }
 
 
 int main()
 {
-    t_projectile *projectile = creat_projectile()
-    return (0);
+    void *mlx;
+    void *win;
+    void *img;
+    t_canvas *canvas;
+    t_tuple  *red;
+
+    mlx = mlx_init();
+    if (!mlx)
+        return(-1);
+    win = mlx_new_window(mlx, 1000, 500, "test");
+    if (!win)
+        return (-2);
+    canvas = creat_canvas(1000, 500, mlx);
+    if (!canvas)
+        return (-3);
+    red = creat_color(1, 1, 1);
+    write_pixel(canvas, 0, 0, red);
+    red = creat_color(1, 0, 1);
+    int x = 1;
+    while( x < 1000)
+    {
+        int y = 1;
+        while(y < 500)
+        {
+            write_pixel(canvas, x, y, red);
+            y++;
+        }
+        x++;
+    }
+    print_type(pixel_at(canvas, 0, 0));
+    print_type(pixel_at(canvas, 1, 1));
+    mlx_put_image_to_window(mlx, win, canvas->img->img, 0, 0);
+    mlx_loop(mlx);
+    
 }
--- miniRT_my/geo/geo.h
+++ miniRT_my/geo/geo.h
@@ -1,6 +1,7 @@
 #ifndef GEO_H
 #define GEO_H
 
+#include "../mlx_linux/mlx.h"
 #include <math.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -10,6 +11,18 @@
 #ifdef EPSILON
 #undef EPSILON
 #endif
+
+#ifdef POINT
+#undef
+#endif
+
+#define POINT 1
+
+#ifdef VEC
+#undef
+#endif
+
+#define VEC 0
 
 #define EPSILON 0.00001
 
@@ -21,6 +34,36 @@ typedef struct s_tuple
     double z;
     int w;
 } t_tuple;
+
+typedef struct projectile
+{
+    t_tuple *position;
+    t_tuple *speed;
+} t_projectile;
+
+typedef struct environment
+{
+    t_tuple *gravity;
+    t_tuple *wind;
+} t_environment;
+
+typedef struct	s_img 
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_img;
+
+typedef struct s_canvas
+{
+    int width;
+    int height;
+    t_img *img;
+} t_canvas;
+
+
 
 t_tuple *creat_tuple(double x, double y, double z, double w);
 t_tuple *creat_vector(double x, double y, double z);
